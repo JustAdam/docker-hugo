7c82fb6599303d8824ab078d68b6413bf093ccfb
JustAdam <adambell7@gmail.com>
2016-10-18 20:41:14 +0200

    Update hugo binary fetching script with new URL

--- gethugo.sh
+++ gethugo.sh
@@ -3,8 +3,9 @@ if [ -z "$HUGO_VERSION" ]; then
   echo Missing version to download
   exit 1
 fi
-wget https://github.com/spf13/hugo/releases/download/v"$HUGO_VERSION"/hugo_"$HUGO_VERSION"_linux_amd64.tar.gz
-tar xvfz hugo_"$HUGO_VERSION"_linux_amd64.tar.gz
+wget -O hugo.tar.gz https://github.com/spf13/hugo/releases/download/v"$HUGO_VERSION"/hugo_"$HUGO_VERSION"_Linux-64bit.tar.gz
+tar xvfz hugo.tar.gz
 mv hugo_"$HUGO_VERSION"_linux_amd64/hugo_"$HUGO_VERSION"_linux_amd64 hugo
 mv hugo_"$HUGO_VERSION"_linux_amd64/LICENSE.md LICENSE.md
 rm -rf hugo_"$HUGO_VERSION"_linux_amd64*
+rm hugo.tar.gz
